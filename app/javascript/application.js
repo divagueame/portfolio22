e9b771f37e146979b9a666e74cd5bba249c33277
Divagueame <martin.arce.teixeira@gmail.com>
2022-07-10 17:20:53 +0200

    Setting up transition

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// const initOverlay = document.querySelector('.overlay');
+// document.addEventListener("DOMContentLoaded", function() {
+//     // initOverlay.classList.add('active')
+    
+// });
