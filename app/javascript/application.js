1cbc391d4ba386e16c97e99a34c8040bf5afc102
Divagueame <martin.arce.teixeira@gmail.com>
2022-07-14 00:37:20 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   app/assets/stylesheets/application.tailwind.css
    	modified:   app/javascript/application.js
    	modified:   app/views/home/index.html.erb

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,3 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-
-// const initOverlay = document.querySelector('.overlay');
-// document.addEventListener("DOMContentLoaded", function() {
-//     initOverlay.classList.add('active')
-    
-// });
