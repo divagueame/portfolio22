d653ece558ab12174c5f0f3ed0865476ae654c30
Divagueame <martin.arce.teixeira@gmail.com>
2022-07-14 22:44:50 +0200

    Add toggle menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+document.querySelector('#toggle-menu').addEventListener('click', toggleMenu)
+document.querySelector('#toggle-menu-mobile').addEventListener('click', toggleMenu)
+ 
+function toggleMenu(e) { 
+        document.querySelector('.menu-mobile').classList.toggle('menu-active');
+        document.querySelector('body').classList.toggle('menu-active');
+}
