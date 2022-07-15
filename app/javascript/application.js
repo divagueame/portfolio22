f1d828ed5998f05c1007f1214d7e75b66755d78b
Divagueame <martin.arce.teixeira@gmail.com>
2022-07-15 17:32:52 +0200

    Intermediate point

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,11 +2,12 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 
-document.querySelector('#toggle-menu').addEventListener('click', toggleMenu)
-document.querySelector('#toggle-menu-mobile').addEventListener('click', toggleMenu)
+document.querySelector('.action-btn').addEventListener('click', toggleMenu)
+// document.querySelector('#toggle-menu').addEventListener('click', toggleMenu)
+// document.querySelector('#toggle-menu-mobile').addEventListener('click', toggleMenu)
  
 function toggleMenu(e) { 
-        console.log('cli')
-        document.querySelector('.menu-mobile').classList.toggle('menu-active');
+        document.querySelector('.menu').classList.toggle('active');
+        // document.querySelector('.menu-mobile').classList.toggle('menu-active');
         // document.querySelector('#content').classList.toggle('menu-active');
 }
