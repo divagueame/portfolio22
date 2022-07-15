f03a51df8147cc63c8a2cc245c5c0ce6c61d6221
Divagueame <martin.arce.teixeira@gmail.com>
2022-07-15 18:38:30 +0200

    Animation

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,14 +3,10 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 
 document.querySelector('.action-btn').addEventListener('click', toggleMenu)
-document.querySelector('.action-btn-2').addEventListener('click', toggleMenu)
-// document.querySelector('#toggle-menu').addEventListener('click', toggleMenu)
-// document.querySelector('#toggle-menu-mobile').addEventListener('click', toggleMenu)
+document.querySelector('.action-btn-2').addEventListener('click', toggleMenu) 
  
 function toggleMenu(e) { 
         document.querySelector('.menu').classList.toggle('active');
         document.querySelector('.content').classList.toggle('inactive');
-        document.querySelector('.action-btn').classList.toggle('inactive');
-        // document.querySelector('.menu-mobile').classList.toggle('menu-active');
-        // document.querySelector('#content').classList.toggle('menu-active');
+        document.querySelector('.action-btn').classList.toggle('inactive'); 
 }
