afe82403fea4775c03e5078aeae925fbdf057c0b
Divagueame <martin.arce.teixeira@gmail.com>
2022-07-15 17:50:54 +0200

    Testing

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,6 +8,7 @@ document.querySelector('.action-btn').addEventListener('click', toggleMenu)
  
 function toggleMenu(e) { 
         document.querySelector('.menu').classList.toggle('active');
+        document.querySelector('.content').classList.toggle('inactive');
         // document.querySelector('.menu-mobile').classList.toggle('menu-active');
         // document.querySelector('#content').classList.toggle('menu-active');
 }
